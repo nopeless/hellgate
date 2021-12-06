04a33ecfcea849273d971d3023232f6881ca16b6
nopeless <38830903+nopeless@users.noreply.github.com>
2021-12-07 01:10:33 +0900

    add ssg spec

---
+++ test/ssg/main.spec.js
@@ -0,0 +1,188 @@
+const { expect } = require(`chai`);
+
+const { Hellgate, Ring, IHotel } = REQUIRE_HELLGATE;
+
+class User {
+  constructor(name, props = {}) {
+    this.name = name;
+    this.props = {
+      balance: 0,
+      rank: `customer`,
+      ...props,
+    };
+  }
+}
+
+const ranks = [
+  [`normal`, 400],
+  [`vip`, 800],
+  [`vvip`, 1600],
+  [`vvvip`, 4000],
+  [`mvp`, 10_000],
+  [`diamond`, 50_000],
+  [`black_diamond`, 100_000],
+  [`hogu`, Infinity],
+];
+
+ranks.sort((a, b) => a[1] - b[1]);
+
+
+const statusByBalance = (() => {
+  const status = {};
+  ranks.reduce((a, b) => {
+    status[b[0]] = [a[0]];
+    return b;
+  });
+  return status;
+})();
+
+function getRank(balance) {
+  if (!(balance <= Infinity)) throw new Error(`Balance is not a number`);
+  return ranks.find(v => v[1] > balance)[0];
+}
+
+describe(`getRank function`, function() {
+  it(`Should work`, function() {
+    expect(getRank(0)).to.equal(`normal`);
+    expect(getRank(400)).to.equal(`vip`);
+    expect(getRank(401)).to.equal(`vip`);
+    expect(getRank(800)).to.equal(`vvip`);
+    expect(getRank(801)).to.equal(`vvip`);
+    expect(getRank(1600)).to.equal(`vvvip`);
+    expect(getRank(1601)).to.equal(`vvvip`);
+    expect(getRank(4000)).to.equal(`mvp`);
+    expect(getRank(4001)).to.equal(`mvp`);
+    expect(getRank(10000)).to.equal(`diamond`);
+    expect(getRank(10001)).to.equal(`diamond`);
+    expect(getRank(50000)).to.equal(`black_diamond`);
+    expect(getRank(50001)).to.equal(`black_diamond`);
+    expect(getRank(100000)).to.equal(`hogu`);
+    expect(getRank(100001)).to.equal(`hogu`);
+  });
+});
+
+const statusMap = {
+  boss: [`shop_owner`],
+  shop_owner: [`top_manager`],
+  top_manager: [`manager`],
+  manager: [`customer`],
+  parking: [],
+  customer: [],
+  ...statusByBalance,
+};
+
+const maff = new User(`Maff Bezos`, {
+  balance: 1e10,
+});
+
+const BJ = new User(`Mr. BJ`, {
+  balance: 5000,
+});
+
+const apple = new User(`Apple`, {
+  balance: 500,
+});
+
+const rika = new User(`Rika`, {
+  balance: 1,
+});
+
+const nullUser = new User(`null`, {
+  rank: ``,
+});
+
+const users = [maff, BJ, apple, rika, nullUser];
+
+function findUser(name) {
+  const i = users.find(v => v.name.toLowerCase().includes(name));
+  if (!i) {
+    throw new Error(`User not found`);
+  }
+  return i;
+}
+
+class UserResolve extends IHotel {
+  user(user) {
+    if (typeof user !== `string`) {
+      return super.user(user);
+    }
+    const obj = findUser(user);
+    const rank = getRank(obj.props.balance);
+    return super.user(obj, [obj.props.rank, rank]);
+  }
+}
+
+const userResolve = new UserResolve();
+
+userResolve.loadStatusMap(statusMap);
+
+const mall = new Hellgate(userResolve, new Ring(null, {
+  enter: false,
+}, {
+  enter: [`customer`],
+}));
+
+const rounge = new Ring(mall, {
+  order: function (user, _, food) {
+    if (!food) throw new Error(`Food is not defined`);
+    if (!this.canSync(user, `enter`)) return false;
+
+    if ([`cookie`, `apple`].includes(food)) {
+      return true;
+    }
+    if ([`diamond`].includes(food)) {
+      return this.hasStatus(user, `diamond`);
+    }
+    return false;
+  },
+});
+
+class PrivateRoom extends Ring {
+  constructor(allowedPeople = [], ...args) {
+    super(...args);
+    this.allowedPeople = allowedPeople;
+  }
+}
+
+function createPrivateRoom(allowed) {
+  const room = new PrivateRoom(allowed, rounge, {
+    enter: function(user) {
+      const name = user.name.toLowerCase();
+      return this.allowedPeople.some(v => name.toLowerCase().includes(v));
+    },
+  });
+  return room;
+}
+
+describe(`Main`, function() {
+  it(`Should process basic functions`, function() {
+    expect(mall.canSync(`null`, `enter`)).to.be.false;
+    expect(mall.canSync(`maff`, `enter`)).to.be.true;
+    expect(mall.canSync(`bj`, `enter`)).to.be.true;
+    expect(mall.canSync(`apple`, `enter`)).to.be.true;
+    expect(mall.canSync(`rika`, `enter`)).to.be.true;
+  });
+  it(`Normal customers cannot enter VIP Parking room`, function() {
+    expect(() => rounge.canSync(`maff`, `order`)).to.throw;
+    expect(rounge.canSync(`null`, `order`, `cookie`)).to.be.false;
+    expect(rounge.canSync(`bj`, `order`, `cookie`)).to.be.true;
+    expect(rounge.canSync(`bj`, `order`, `diamond`)).to.be.false;
+    expect(rounge.canSync(`maff`, `order`, `cookie`)).to.be.true;
+    expect(rounge.canSync(`maff`, `order`, `diamond`)).to.be.true;
+  });
+  describe(`Private room`, function() {
+    let room;
+    before(() => {
+      room = createPrivateRoom([`maff`, `bj`]);
+    });
+    after(() => {
+      room.destroy();
+    });
+    it(`Make new VIP room`, function() {
+      expect(room.canSync(`maff`, `enter`)).to.be.true;
+      expect(room.canSync(`bj`, `enter`)).to.be.true;
+      expect(room.canSync(`rika`, `enter`)).to.be.false;
+      expect(room.canSync(`null`, `enter`)).to.be.false;
+    });
+  });
+});
